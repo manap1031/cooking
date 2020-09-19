0cd3ea27ca59908c509f9375a210b90af3686b5c
manap1031 <ikme.sabosan00@gmail.com>
2020-09-19 20:11:32 +0900

    投稿画面の実装

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -2,4 +2,24 @@ class CooksController < ApplicationController
   def index
     @cooks = Cook.all
   end
+
+  def new
+    @cook = Cook.new
+  end
+
+  def create
+    @cook = Cook.new(cook_params)
+    if @cook.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cook_params
+    params.require(:cook).permit(:recipe_name, :recipe, :image)
+  end
+
 end
