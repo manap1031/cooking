ee70c383546f7ea2769b3fe7dee0aa5bd41d4a9e
manap1031 <ikme.sabosan00@gmail.com>
2020-09-19 19:09:44 +0900

    フロント画面の実装

---
+++ db/migrate/20200919093425_create_cooks.rb
@@ -0,0 +1,9 @@
+class CreateCooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cooks do |t|
+      t.string :recipe_name
+      t.string :recipe
+      t.timestamps
+    end
+  end
+end
