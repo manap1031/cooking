17a2fdac657b482a85f530309021be0d28222ce1
manap1031 <ikme.sabosan00@gmail.com>
2020-09-21 20:05:13 +0900

    修正

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,5 +1,5 @@
 class CooksController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :create]
 
   def index
     @cooks = Cook.includes(:user).order('created_at DESC')
